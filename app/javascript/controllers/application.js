bdbf27e1cf55bba58df7e182727cd3cac5651845
Ethan Marrs <ethanmarrs@gmail.com>
2022-09-14 10:06:16 +0200

    Remove old code

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import ResetFormController from "./form_controller"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
-
-application.register("form", ResetFormController)
+window.Stimulus = application
 
 export { application }
