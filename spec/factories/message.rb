f61b349724b5e6751ce7dc6f89ae0fef47e76ced
Ethan Marrs <ethanmarrs@gmail.com>
2022-09-14 11:48:25 +0200

    Add query spec for conversation

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :message do
     text { "Hi, how are you?" }
     from { :bot }
+    conversation
   end
 end
--- spec/support/helpers/graphql_helpers.rb
+++ spec/support/helpers/graphql_helpers.rb
@@ -5,4 +5,8 @@ module GraphqlHelpers
   def self.fieldnamerize(underscored_field_name)
     underscored_field_name.to_s.camelize(:lower)
   end
+
+  def self.to_snake_case(hash)
+    hash.deep_transform_keys { |key| key.to_s.underscore.to_sym }
+  end
 end
