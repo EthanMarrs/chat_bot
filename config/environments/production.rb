f94825cb7a5f2be466424f9b664628295b51a1e1
Ethan Marrs <ethanmarrs@gmail.com>
2022-09-16 12:39:34 +0200

    Use credentials file for hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts = [
-    ".ethanmarrs.dev",
-    "chat-bot-zvo9.onrender.com"
-  ]
+  config.hosts = Rails.application.credentials.hosts
 end
